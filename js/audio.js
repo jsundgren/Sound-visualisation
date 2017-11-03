0bead9e4ffbe3770f1355089153ce14008fb4349
Johan Sundgren <johan.sundgren@hotmail.com>
2017-11-03 22:35:41 +0100

    change in UI

--- js/audio.js
+++ js/audio.js
@@ -7,11 +7,11 @@ var col = {
 };
 
 function preload(){
-    song = loadSound('mm.mp3');
+    song = loadSound('mm.ogg');
 }
 
 function setup(){
-    createCanvas(windowWidth,windowHeight, WEBGL);
+    createCanvas(0.75*windowWidth,0.75*windowHeight, WEBGL);
     song.loop();
     analyzer = new p5.Amplitude();
     analyzer.setInput(song);
@@ -19,9 +19,11 @@ function setup(){
 
 function draw(){
     background(234,165,255);
+    translate(0,0,-300);
+    push();
     rotateY(frameCount * 0.01);
     var rms = analyzer.getLevel();
-    translate(100,100);
+    translate(50,50);
     rotateX(frameCount * 0.01);
     for(var j = 0; j < 6; j++){
         push();
@@ -29,9 +31,14 @@ function draw(){
             translate(sin(frameCount * 0.0001 + j) * 200, cos(frameCount * 0.0001 + j) * 200, i * 0.5);
             rotateZ(frameCount * 0.002);
             push();
-            sphere(10+rms*30); 
+            sphere(5+rms*30); 
             pop();
         }
         pop();
     }
+    pop();
+}
+
+function windowResized() {
+  resizeCanvas(0.75*windowWidth, 0.75*windowHeight);
 }
